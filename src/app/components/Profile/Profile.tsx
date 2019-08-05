dea918f2dbefa70c985c00f6c312ddbd6161363f
Dawid <wizard91@o2.pl>
2019-08-05 23:49:15 +0100

    removed unneeded data

--- src/app/components/Profile/Profile.tsx
+++ src/app/components/Profile/Profile.tsx
@@ -7,7 +7,7 @@ import { changeStat } from '../../actions';
 // import { input } from './../../models/Input';
 
 const styles = require('../../Scss/main.css');
-const profile = new Set<string>(['hp', 'exp', 'gold', 'mana']);
+const profile = new Set<string>(['hp', 'exp', 'gold']);
 
 interface ProfileInterface extends RouteComponentProps, InjectedFormProps {
   profile: {[key:string]:number};
