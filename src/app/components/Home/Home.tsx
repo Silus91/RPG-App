74fce595a799ba9bfed3d601c89127baa4377cc5
Dawid <wizard91@o2.pl>
2019-08-14 22:09:03 +0100

    dice in home

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -7,6 +7,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <h1> RPG HELPER!</h1>
+      <img  src={require(`./../Dice/DiceD6.png`)} className={styles.img}/>
       <div className={styles.appContainer}>
         <Link to='/dice'><div className={styles.button}>Dice</div></Link>
         <Link to={'/todoMain'}><div className={styles.button}>Quests</div></Link>
