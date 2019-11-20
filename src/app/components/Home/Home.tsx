b95a6cfb7952440968468fcc76de4407d9b810ac
Dawid <wizard91@o2.pl>
2019-11-20 22:03:29 +0000

    spare brackets delete

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -10,9 +10,9 @@ const Home = () => {
       <img  src={require(`./../Dice/DiceD6.png`)} className={styles.img}/>
       <div className={styles.appContainer}>
         <Link to='/dice'><div className={styles.button}>Dice</div></Link>
-        <Link to={'/todoMain'}><div className={styles.button}>Quests</div></Link>
-        <Link to={'/profile'}><div className={styles.button}>Profile</div></Link>
-        <Link to={'/challange'}><div className={styles.button}>Challange</div></Link>
+        <Link to='/todoMain'><div className={styles.button}>Quests</div></Link>
+        <Link to='/profile'><div className={styles.button}>Profile</div></Link>
+        <Link to='/challange'><div className={styles.button}>Challange</div></Link>
 
       </div>
     </div>
