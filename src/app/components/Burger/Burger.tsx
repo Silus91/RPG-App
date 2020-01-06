f97fe2b0d0dd8f8b7214bd4167f7194fd611f6a9
Dawid <wizard91@o2.pl>
2020-01-06 23:18:22 +0000

    removing burger

--- src/app/components/Burger/Burger.tsx
+++ src/app/components/Burger/Burger.tsx
@@ -6,18 +6,7 @@ const styles = require('../../Scss/main.css');
 class Burger extends React.Component<any,any> {
 
 
- renderBurger() {
-   return (
-      <div className={styles.navigation}>
-        <ul className={styles.navigation__list}>
-          <li className={styles.navigation__item}><a href="">Lorem</a></li>
-          <li className={styles.navigation__item}><a href="">Ipsum</a></li>
-          <li className={styles.navigation__item}><a href="">Dolor</a></li>
-          <li className={styles.navigation__item}><a href="">Sit</a></li>
-        </ul>
-      </div>
-   )
- } 
+ 
     render(){
       return(
       <div>
