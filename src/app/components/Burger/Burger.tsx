c6f3580b6123aeaabcdfcefc7b4208d4e20e4d1c
Dawid <wizard91@o2.pl>
2020-01-07 08:21:40 +0000

    removing burger.tsx

--- src/app/components/Burger/Burger.tsx
+++ src/app/components/Burger/Burger.tsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-// import { RouteComponentProps } from 'react-router-dom';
-const styles = require('../../Scss/main.css');
-
 
 class Burger extends React.Component<any,any> {
 
@@ -10,13 +7,7 @@ class Burger extends React.Component<any,any> {
     render(){
       return(
       <div>
-        <button  className={styles.hamburger}>
-          <span className={styles.hamburger__box}>
-            <span className={styles.hamburger__inner}>
-              {this.renderBurger()}
-            </span>
-          </span>
-        </button>
+
       </div>
 
       )
