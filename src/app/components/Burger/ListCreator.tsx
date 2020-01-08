8f61b84ff2e40cc6f8e2e9a348b9a94776c88a32
Dawid <wizard91@o2.pl>
2020-01-08 23:21:36 +0000

    burger removed added ListCreator

---
+++ src/app/components/Burger/ListCreator.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ListCreator extends Component {
+  render() {
+    return (
+      <div>
+        <p>1</p>
+      </div>
+    )
+  }
+}
+
+export default ListCreator;
